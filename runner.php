9b02b3aeb4cece4442b51e466d51a8fecbb5b513
François de Metz <fdemetz@af83.com>
2010-02-05 16:06:27 +0100

    updated README, added some comments, and introduced SimpleTestRunner_Observer

--- runner.php
+++ runner.php
@@ -1,37 +1,40 @@
 <?php
 /**
- *  Copyright (c) 2010, AF83
- *  All rights reserved.
+ * Custom runner for simpletest
+ * Copyright (C) 2010 AF83
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- *  Redistribution and use in source and binary forms, with or without modification,
- *  are permitted provided that the following conditions are met:
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- *  1° Redistributions of source code must retain the above copyright notice,
- *  this list of conditions and the following disclaimer.
- *
- *  2° Redistributions in binary form must reproduce the above copyright notice,
- *  this list of conditions and the following disclaimer in the documentation
- *  and/or other materials provided with the distribution.
- *
- *  3° Neither the name of AF83 nor the names of its contributors may be used
- *  to endorse or promote products derived from this software without specific
- *  prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COMPANY AF83 AND CONTRIBUTORS "AS IS"
- *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- *  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- *  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- *  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+/**
+ *
+ */
+interface SimpleTestRunner_Observer 
+{
+    public function render();
+}
+
+/**
+ * Simple Wrapper around TestSuite
+ */
 class SimpleTestRunner_TestSuite extends TestSuite
 {
+    /**
+     * Get all available tests from this suite
+     * @return array
+     */
     public function getAllTests()
     {
         $tests = array();
@@ -47,9 +50,11 @@ class SimpleTestRunner_TestSuite extends TestSuite
     } 
 }
 
+/**
+ * Custom reporter with form to select testcase
+ */
 class SimpleTestRunner_Form extends SimpleTestRunner_CustomDisplay
 {
-
     public function __construct()
     {
         parent::__construct();
@@ -75,10 +80,6 @@ EOS;
         {
             $this->makeDry();
         }
-        else
-        {
-
-        }
         $test_cases = self::$allTests;
         $form = '<form action="" method="get">';
         foreach($test_cases as $suite => $tests)
@@ -98,6 +99,9 @@ EOS;
 
 }
 
+/**
+ * 
+ */
 class SimpleTestRunner_CustomDisplay extends HtmlReporter
 {
     public static $allTests = null;
@@ -108,11 +112,10 @@ class SimpleTestRunner_CustomDisplay extends HtmlReporter
     protected $test_case_name = null;
     protected $nb_errors = 0;
 
-    public function __construct()
-    {
-        parent::HtmlReporter();
-    }
-
+    /**
+     * Invoke only test selected
+     * @return Boolean
+     */
     public function shouldInvoke($test_case_name, $method)
     {
         $this->test_case_name = $test_case_name;
@@ -136,8 +139,10 @@ class SimpleTestRunner_CustomDisplay extends HtmlReporter
             <title>Tests</title>
             <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
             <style type="text/css">
-            .fail { background-color: inherit; color: red; }.pass { background-color: inherit; color: green; } pre { background-color: lightgray; color: inherit; }
-        .tests { width: 400px; margin-right: 5px; padding: 3px;font-size: 0.9em; float: left; background-color: #BFBFBF;}
+            .result { padding: 8px; margin-top: 1em;  color: white;}
+            .fail { background-color: red;}
+            .pass { background-color: green;} pre { background-color: lightgray; color: inherit; }
+            .tests { width: 400px; margin-right: 5px; padding: 3px;font-size: 0.9em; float: left; background-color: #BFBFBF;}
             </style>
             <script type="text/javascript" src="/js/prototype.js"></script>
             <script type="text/javascript" src="/js/scriptaculous.js"></script>
@@ -198,11 +203,11 @@ EOS;
 EOS;
         if($this->methodDidNotFailYet())
         {
-            echo "<div style=\"padding: 8px; margin-top: 1em; background-color: green; color: white;\">Test passed \o/</div>";
+            echo "<div class=\"result pass\">Test passed \o/</div>";
         }
         else
         {
-            echo "<div style=\"padding: 8px; margin-top: 1em; background-color: red; color: white;\"><blink>Test failed :'(</blink></div>";
+            echo "<div class=\"result fail\"><blink>Test failed :'(</blink></div>"; // yes we love blink ;)
         }
         array_pop($this->_test_stack);
         flush();
@@ -218,7 +223,11 @@ EOS;
         return false;
     }
 
-    public static function setObserver(HtmlRequestObserver $observer)
+    /**
+     * Add observer called after each test
+     * Useful with an http request observer
+     */
+    public static function setObserver(SimpleTestRunner_Observer $observer)
     {
         self::$observer = $observer;
     }
